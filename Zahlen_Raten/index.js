c3f9d3360773b753fb9335be43f5740b222ca4dc
donatoflemma <tato.flemma@gmail.com>
2025-03-13 19:52:29 +0100

    i try same code

--- Zahlen_Raten/index.js
+++ Zahlen_Raten/index.js
@@ -1,22 +1,26 @@
 let number;
+
 list = []
 
 function startGuessNumbers(){
     var n = Math.random() * 100;
         n = Math.ceil(n);
         number = n;   
-        list.push(n)
-        console.log(number);
+        //list.push(n)
+        //console.log(number);
 }
     
 
 
-function checkInput(){
-   guestNumber= parseInt(document.getElementById("input").value); // prendo l´imput e lo rendo int 
-   alert(list[0]);
-
-   console.log(guestNumber);
-    if (number == guestNumber){
-    document.getElementById("answer");
-   }
+function checkInput() {
+   alert("cisao");
+   console.log("gu");
 }
+
+
+    //alert(list[0]);
+
+   //console.log(guestNumber);
+    //if (number == guestNumber){
+    //document.getElementById("answer");
+   
