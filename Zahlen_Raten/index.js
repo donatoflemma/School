8c7bf5c3a4010a142dfa9f2de2a79784ec4f450a
donatoflemma <tato.flemma@gmail.com>
2025-03-14 10:16:24 +0100

    i complete to coding , all works

--- Zahlen_Raten/index.js
+++ Zahlen_Raten/index.js
@@ -1,26 +1,47 @@
 let number;
-
-list = []
-
+let tries =0;
 function startGuessNumbers(){
     var n = Math.random() * 100;
         n = Math.ceil(n);
-        number = n;   
-        //list.push(n)
-        //console.log(number);
+        number = n; 
+        alert("                                       Ready !!! \n                         10 tries than Game Over \n                                    Good Luck !!");
+        document.getElementById("game-text2").textContent = "";  
 }
     
-
-
 function checkInput() {
-   alert("cisao");
-   console.log("gu");
+    event.preventDefault();
+    let answer = parseInt(document.getElementById("input").value);
+    if (tries !== 10){
+        document.getElementById("Lose").textContent = "";
+        console.log(answer);
+        console.log(number);
+        if(number == answer){
+            document.getElementById("game-text2").textContent = "Won !!";
+            document.getElementById("game-text2").style.color = "green"
+            console.log(tries);
+        }
+        else if (number !== answer && number > answer){
+            document.getElementById("game-text2").textContent = "Try agein" + "your number is too small ";
+            document.getElementById("game-text2").style.color = "red"
+            tries ++
+            console.log(tries);
+        }
+        else if (number !== answer && number < answer){
+            document.getElementById("game-text2").textContent = "Try agein \n" + "your number is too big ";
+            document.getElementById("game-text2").style.color = "blue"
+            tries ++
+            console.log(tries);
+        }
+    }
+    else{
+        document.getElementById("Lose").textContent = "Yuo Lose !!! Push the window for restart the play";
+        document.getElementById("game-text2").textContent = "";
+        number = 0;
+        tries = 0;
+    }
+
 }
 
 
-    //alert(list[0]);
-
-   //console.log(guestNumber);
-    //if (number == guestNumber){
-    //document.getElementById("answer");
+   
    
