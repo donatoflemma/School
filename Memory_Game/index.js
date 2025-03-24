a542973e16a25e4b8d08dcd3a6c691bc157b18a3
donatoflemma <tato.flemma@gmail.com>
2025-03-24 08:39:09 +0100

    new Project

---
+++ Memory_Game/index.js
@@ -0,0 +1,48 @@
+let Animols = ["bear.gif","cat.gif","elephant.gif","fox.gif","goose.gif","sheep.gif","sparrow.gif","wolf.gif"];
+let Geometrics = [];
+let Vegetables = [];
+
+
+function Select(){
+// Funzione per fare il gioco delle carte 
+}
+
+function Reset(){
+// Funzione per far ripartire il gioco     
+}
+
+function Change(){
+// funzione per cambiare le carte con cui si gioca , capire se serve una 
+// sola funzione per ogni tipo di carta o una funzione per ogniuna  
+}
+   /* transform-style: preserve-3d;
+    transition: transform 0.6s ease-in-out;*/
+//}
+/*.card.flipped {
+    transform: rotateY(180deg);
+  }*/
+  
+/*  .card-front, .card-back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    backface-visibility: hidden;
+  }
+  
+  .card-back {
+    transform: rotateY(180deg);
+  }
+    
+  
+
+
+<div class="card-container" onclick="flipCard(this)">
+  <div class="card">
+    <div class="card-face card-front">FRONT</div>
+    <div class="card-face card-back">BACK</div>
+  </div>
+</div>
+  
+  */
+
+ 
